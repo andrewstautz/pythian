e1ba703ca725d1e9d79691745aee6c7d4fbc0008
Andrew Stautz <astautz@tccrocks.com>
2018-03-16 15:24:36 -0400

    making calculation script wait for job result

--- app.py
+++ app.py
@@ -266,8 +266,9 @@ def run_calculation(n_clicks, forecast_periods, contents, filename):
         data = parse_contents(contents, filename)
         q = Queue(connection=conn)
         job = q.enqueue_call(func=main_script, args=(data, forecast_periods))
-        calculated_data = job.result
-        return codecs.encode(pickle.dumps(calculated_data), "base64").decode()
+        if job.result is not None:
+            calculated_data = job.result
+            return codecs.encode(pickle.dumps(calculated_data), "base64").decode()
 
 #
 # To decode string in hidden div:

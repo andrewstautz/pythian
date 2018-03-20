5c59f6141ceb3ea693ce90dbe4bb3eeb28966872
Steve McQuaid <steve@stevemcquaid.com>
2018-03-20 10:38:56 -0400

    Change scripts to not use bash

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,2 @@
-#!/bin/bash
-set -ex
-
-source scripts/common.sh
-
-docker run -it -p 80:8050 -v $PWD:/src --rm stevemcquaid/$PACKAGE_NAME:latest python app.py
+docker run -it -p 80:8050 -v $PWD:/src --rm stevemcquaid/pythian:latest python app.py
 

3ba787698f3d5d9392fc03ecf429993346162c63
Steve McQuaid <steve@stevemcquaid.com>
2018-03-20 10:21:57 -0400

    Expose port 80

--- scripts/run.sh
+++ scripts/run.sh
@@ -3,5 +3,5 @@ set -ex
 
 source scripts/common.sh
 
-docker run -it -p 8050:8050 -v $PWD:/src --rm stevemcquaid/$PACKAGE_NAME:latest python app.py
+docker run -it -p 80:8050 -v $PWD:/src --rm stevemcquaid/$PACKAGE_NAME:latest python app.py
 
